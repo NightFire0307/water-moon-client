e7d541634da766ff9d783ea3c2a19afa493cd99c
繁花落叶已成霜 <1035612457@qq.com>
2025-01-01 00:24:52 +0800

    新增React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App.tsx'
+import { Login } from './views/login'
 import './index.css'
 import './assets/normal.css'
 
 createRoot(document.getElementById('root')!).render(
-  <ConfigProvider
-    locale={zhCN}
-  >
-    <App />
-  </ConfigProvider>,
+  <BrowserRouter>
+    <ConfigProvider
+      locale={zhCN}
+    >
+      <Routes>
+        <Route path="/">
+          <Route path=":token" element={<App />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </ConfigProvider>
+  </BrowserRouter>,
 )
