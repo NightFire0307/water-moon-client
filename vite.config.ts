7338c270b2c1d780c8d7788576c980c522b18be2
繁花落叶已成霜 <1035612457@qq.com>
2025-03-20 15:09:54 +0800

    feat: 新增vite-plugin-svgr插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({
+    svgrOptions: {
+      icon: true,
+    },
+  })],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
   server: {
+    port: 5174,
     proxy: {
       // '/api': 'https://apifoxmock.com/m1/4320757-3963744-default',
       '/api': {
