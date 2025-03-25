73f39cce4c0ab52913c64f036fe0651574e8ad07
繁花落叶已成霜 <1035612457@qq.com>
2025-03-25 11:50:14 +0800

    完善选片完成提交框二次确认功能

--- src/views/home/components/ConfirmModal.tsx
+++ src/views/home/components/ConfirmModal.tsx
@@ -1,8 +1,11 @@
-import type { FC } from 'react'
+import type { IProduct } from '@/stores/productsStore.tsx'
+import type { FC, ReactElement } from 'react'
 import { useProductsStore } from '@/stores/productsStore.tsx'
-import { LockOutlined, WarningOutlined } from '@ant-design/icons'
+import { CheckCircleOutlined, LockOutlined, WarningOutlined } from '@ant-design/icons'
 import { Alert, Button, ConfigProvider, Modal, Space } from 'antd'
 import { createStyles } from 'antd-style'
+import cs from 'classnames'
+import { useMemo } from 'react'
 
 interface ConfirmModalProps {
   open: boolean
@@ -11,27 +14,31 @@ interface ConfirmModalProps {
   onCancel: () => void
 }
 
+interface AlertContentProps {
+  title: string
+  type: 'success' | 'warning'
+  icon?: ReactElement
+  unFinishedProducts?: IProduct[]
+}
+
 // 警告内容
-const AlertContent: FC = () => {
-  const products = useProductsStore(state => state.products)
-
-  // 未完成的选片
-  const unFinishedProducts = products.filter(product => product.photo_limit > product.selected_photos.length)
-
+const AlertContent: FC<AlertContentProps> = ({ title, type, icon, unFinishedProducts }) => {
   return (
-    <div className="flex items-start gap-2 text-amber-600 font-medium">
+    <div className={cs('flex gap-2 font-medium', type === 'success' ? 'text-emerald-600 items-center' : 'text-amber-600 items-start')}>
       <span className="text-xl ">
-        <WarningOutlined />
+        {
+          icon || (type === 'success' ? <CheckCircleOutlined /> : <WarningOutlined />)
+        }
       </span>
       <div>
-        <div className="text-base leading-8">以下产品选片进度未达到100%</div>
+        <div className="text-base leading-8">{title}</div>
         {
-          unFinishedProducts.map(product => (
+          unFinishedProducts && unFinishedProducts.map(product => (
             <div key={product.productId} className="flex justify-between">
               <span>{product.title}</span>
               <div>
                 {
-                  `${product.selected_photos.length} / ${product.photo_limit} (${(product.selected_photos.length / product.photo_limit) * 100})%`
+                  `${product.selected_photos.length} / ${(product.photo_limit * product.count)} (${(product.selected_photos.length / (product.photo_limit * product.count)) * 100})%`
                 }
               </div>
             </div>
@@ -82,7 +89,14 @@ const useStyle = createStyles(({ prefixCls, css }) => ({
 
 export function ConfirmModal(props: ConfirmModalProps) {
   const { open, onCancel, onSubmit, confirmLoading } = props
+  const products = useProductsStore(state => state.products)
   const { styles } = useStyle()
+
+  // 未完成选片的产品
+  const unFinishedProducts = useMemo(
+    () => products.filter(product => (product.photo_limit * product.count) > product.selected_photos.length),
+    [products],
+  )
 
   return (
     <ConfigProvider
@@ -93,8 +107,8 @@ export function ConfirmModal(props: ConfirmModalProps) {
       <Modal
         footer={(
           <Space>
-            <Button>取消</Button>
-            <Button type="primary">确认提交</Button>
+            <Button onClick={() => onCancel()}>取消</Button>
+            <Button type="primary" onClick={() => onSubmit()} loading={confirmLoading}>确认提交</Button>
           </Space>
         )}
         open={open}
@@ -108,9 +122,13 @@ export function ConfirmModal(props: ConfirmModalProps) {
           <span className="text-xl font-bold">确认提交选片结果</span>
         </div>
 
-        <div className="my-1 text-darkBlueGray-600">提交后将进入预览模式，选片结果将被锁定，无法再进行修改。</div>
+        <div className="mt-1 mb-4 text-darkBlueGray-600">提交后将进入预览模式，选片结果将被锁定，无法再进行修改。</div>
 
-        <Alert type="warning" message={<AlertContent />} />
+        {
+          unFinishedProducts.length > 0
+            ? <Alert type="warning" message={<AlertContent title="以下产品选片进度未达到100%" type="warning" unFinishedProducts={unFinishedProducts} />} />
+            : <Alert type="success" message={<AlertContent title="所有产品均已完成选片" type="success" />} />
+        }
       </Modal>
     </ConfigProvider>
   )
