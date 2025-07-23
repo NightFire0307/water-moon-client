03eb6a496a8c6168b71055c74ca3a93ef9a354e6
繁花落叶已成霜 <1035612457@qq.com>
2025-07-23 23:53:19 +0800

    feat: 添加加载状态显示，优化图片加载体验

--- src/components/MainViewer/MainViewer.tsx
+++ src/components/MainViewer/MainViewer.tsx
@@ -1,10 +1,12 @@
 import { usePhotoViewerContext } from '@/contexts/PhotoViewerContext'
 import { usePhotosStore } from '@/stores/usePhotosStore'
 import { usePhotoViewerStore } from '@/stores/usePhotoViewerStore'
+import { LoadingOutlined } from '@ant-design/icons'
 
 export function MainViewer() {
   const { viewerControlVisible, setViewerControlVisible } = usePhotoViewerContext()
   const currentPhoto = usePhotosStore(state => state.currentPhoto)
+  const isLoading = usePhotosStore(state => state.isLoading)
   const { rotate, scale } = usePhotoViewerStore()
 
   return (
@@ -12,14 +14,26 @@ export function MainViewer() {
       className="h-screen select-none overflow-hidden"
       onClick={() => setViewerControlVisible(!viewerControlVisible)}
     >
-      <img
-        src={currentPhoto?.thumbnail_url}
-        alt={currentPhoto?.name}
-        className="w-full h-full object-contain transition-transform duration-300 ease-in-out"
-        style={{
-          transform: `rotate(${rotate}deg) scale(${scale})`,
-        }}
-      />
+      {
+        isLoading
+          ? (
+              <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+                <LoadingOutlined className="text-3xl text-darkBlueGray-600" spin />
+                <span className="text-darkBlueGray-400 font-medium">图片载入中...</span>
+              </div>
+            )
+          : (
+              <img
+                src={currentPhoto?.thumbnail_url}
+                alt={currentPhoto?.name}
+                className="w-full h-full object-contain transition-transform duration-300 ease-in-out"
+                style={{
+                  transform: `rotate(${rotate}deg) scale(${scale})`,
+                }}
+              />
+            )
+      }
+
     </div>
   )
 }
